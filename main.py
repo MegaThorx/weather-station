2685baa5a4e29e6de3eef1bf7515220571605d5f
MegaThorx <megathorx@gmail.com>
2018-05-22 11:18:47 +0200

    Changed rendering to image

--- main.py
+++ main.py
@@ -24,14 +24,13 @@
  # THE SOFTWARE.
  ##
 
-import epd7in5
+#import epd7in5
 import Image
 import ImageDraw
 import ImageFont
 import time
 import locale
 import forecast
-#import imagedata
 
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
@@ -40,18 +39,27 @@ EPD_HEIGHT = 384
 # https://api.openweathermap.org/data/2.5/forecast?q=Innsbruck&units=metric&appid=2678fc5edec0fa22ba8f9d60b5085edc
 # https://api.openweathermap.org/data/2.5/weather?q=Innsbruck&units=metric&appid=2678fc5edec0fa22ba8f9d60b5085edc
 def main():
-    locale.setlocale(locale.LC_ALL, 'de_AT.utf8')
+    # locale.setlocale(locale.LC_ALL, 'de_AT.utf8')
+    draw_image()
+    """epd = epd7in5.EPD()
+    epd.init()
+
+    epd.display_frame(epd.get_frame_buffer(image))"""
+
+def draw_image():
     temp = forecast.get_current_weather()
     forecasts = forecast.get_forecast()
-    epd = epd7in5.EPD()
-    epd.init()
 
     # For simplicity, the arguments are explicit numerical coordinates
     image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
-    draw.rectangle((0, 0, EPD_WIDTH, EPD_HEIGHT), fill=0)
-    draw.rectangle((10, 10, EPD_WIDTH - 10, EPD_HEIGHT - 10), fill=255)
+
+
+    draw.rectangle((0, 0, EPD_WIDTH, EPD_HEIGHT), fill=0) # make it black
+    draw.rectangle((3, 3, EPD_WIDTH - 3, EPD_HEIGHT - 3), fill=255)
+
+
     draw.rectangle((214, 10, 216, EPD_HEIGHT - 10), fill=0)
     draw.rectangle((422, 10, 424, EPD_HEIGHT - 10), fill=0)
     draw.text((20, 20), str(temp) + u" \u00B0" + "C", font=font, fill=0)
@@ -73,7 +81,7 @@ def main():
     date = date + "\n" + time.strftime("%A")
 
     draw.multiline_text((460, 20), date, font=font, fill=0, align="center")
-    epd.display_frame(epd.get_frame_buffer(image))
+    image.save("image.bmp", "BMP")
 
 def draw_forecast(draw):
     pass
